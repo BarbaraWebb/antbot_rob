9a7a1ab5b6318ee78fe9383a1516f42dd8bdd154
Robert Mitchell <refmitchell@outlook.com>
2017-10-01 14:33:10 +0100

    Added visualsandbox module to act as a standalone application for the development and testing of vision components

---
+++ visualsandbox/src/main/java/insectrobotics/visualsandbox/StartScreen.java
@@ -0,0 +1,10 @@
+package insectrobotics.visualsandbox;
+
+/**
+ * Start screen for visual sandbox application.
+ * Intended to be a separate application for development and
+ * testing of visual systems and concepts
+ */
+
+public class StartScreen {
+}
